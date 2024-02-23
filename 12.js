092ca6d98b67be4675b0598e65045ec40ea624e5
Maidah208 <161070899+Maidah208@users.noreply.github.com>
2024-02-23 23:46:35 +0500

    Delete 12.js

--- 12.js
+++
@@ -1,5 +0,0 @@
-var my_transportation = ['Honda', 'Civic', 'Corolla', 'revo', 'vigo'];
-for (var _i = 0, my_transportation_1 = my_transportation; _i < my_transportation_1.length; _i++) {
-    var transport = my_transportation_1[_i];
-    console.log('I would like to own a ' + transport + ' car.');
-}
