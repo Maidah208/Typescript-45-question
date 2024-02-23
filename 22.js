be2ffeda4632820b1b087696d0012c0de15dfa4e
Maidah208 <161070899+Maidah208@users.noreply.github.com>
2024-02-23 23:52:34 +0500

    Delete 22.js

--- 22.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-let guest_s = ['Maidah', 'khudaja', 'Mahnoor'];
-// Trying to access an index that doesn't exist (index 3)
-console.log(guest_s[3]);
-//correcting the error
-guests.push('Mirha');
-console.log(guest_s[3]);
