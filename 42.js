e91a15962eee62861f0aa22f3fca5419dfdf3656
Maidah208 <161070899+Maidah208@users.noreply.github.com>
2024-02-23 23:46:19 +0500

    Delete 42.js

--- 42.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-let Magicians = ['Harry', 'David', 'Teller', 'Criss'];
-function make_great(magician) {
-    for (let magician of Magicians) {
-        console.log('Hello ' + magician);
-    }
-}
-make_great(Magicians);
