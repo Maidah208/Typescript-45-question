a8995f66e2c95f1441fc21adcaff23543a05b70a
Maidah208 <161070899+Maidah208@users.noreply.github.com>
2024-02-23 23:51:09 +0500

    Delete 25.js

--- 25.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-let alien_color = 'green';
-if (alien_color == 'green') {
-    console.log('Congratulations! You get 5 points');
-}
-if (alien_color == 'red') {
-    console.log('Congratulations! You get 5 points');
-}
