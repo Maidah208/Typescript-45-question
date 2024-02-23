79a6c1d399b606ed3071601678585289314581e6
Maidah208 <161070899+Maidah208@users.noreply.github.com>
2024-02-23 23:47:14 +0500

    Delete 36-37.js

--- 36-37.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function make_shirt(size = 'large', message = 'I Love Typescript') {
-    console.log('The size of the shirt is \'' + size + '\' and the message on the shirt is \'' + message + '\'');
-}
-make_shirt('medium', 'hello world');
-make_shirt();
-make_shirt('small');
