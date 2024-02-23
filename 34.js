5ca1484701344e926ae04829798fb96ca033a47c
Maidah208 <161070899+Maidah208@users.noreply.github.com>
2024-02-23 23:47:23 +0500

    Delete 34.js

--- 34.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-let pizza = ['pepperoni', 'tikka', 'cheese', 'veggie'];
-for (let favorite of pizza) {
-    console.log('I like ' + favorite + ' pizza.');
-}
-console.log('I really love pizza.');
