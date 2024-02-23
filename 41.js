89a934904e9de233d8ed64ea7d219c6f7ad49563
Maidah208 <161070899+Maidah208@users.noreply.github.com>
2024-02-23 23:46:45 +0500

    Delete 41.js

--- 41.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-let magicians = ['Harry', 'David', 'Teller', 'Criss'];
-function show_magicians(magician) {
-    for (let magician of magicians) {
-        console.log(magician);
-    }
-}
-show_magicians(magicians);
