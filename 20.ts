9702eaf72b63d373e3d222b44d5f0b0a35ac1c3c
Maidah208 <161070899+Maidah208@users.noreply.github.com>
2024-05-25 17:50:06 +0500

    Delete 20.ts

--- 20.ts
+++
@@ -1,5 +0,0 @@
- let historicalPlaces = ['Mohenjo Daro','Minar-e-Pakistan','Katas Raj Temples','Harappa','Lahore Fort','Takht-i-Bahi','Rohtas Fort'];
- console.log('List of Historical Places in pakistan\n');
- for (let place of historicalPlaces){
-    console.log(place);
- }
