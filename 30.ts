a7f7ac5e123c77008a540afe55ee3eecb57cc17a
Maidah208 <161070899+Maidah208@users.noreply.github.com>
2024-05-25 17:44:37 +0500

    Delete 30.ts

--- 30.ts
+++
@@ -1,8 +0,0 @@
-let names= ['Eric','Admin','Henry','Adrian','Lia'];
-for (let name of names ){
-    if (name =='Admin'){
-        console.log('Hello ' + name + ', would you like to see a status report?');
-    }else{
-        console.log('Hello ' + name + ', thankyou for log in again.');
-    }
-}
