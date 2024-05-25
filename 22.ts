7884a4fe83d33f73e5e06d088ac0a1d7f36c9bbc
Maidah208 <161070899+Maidah208@users.noreply.github.com>
2024-05-25 17:46:39 +0500

    Delete 22.ts

--- 22.ts
+++
@@ -1,9 +0,0 @@
-let guest_s = ['Maidah','khudaja','Mahnoor'];
-
-// Trying to access an index that doesn't exist (index 3)
-console.log(guest_s[3]);
-
-//correcting the error
-guests.push('Mirha');
-
-console.log(guest_s[3]);
