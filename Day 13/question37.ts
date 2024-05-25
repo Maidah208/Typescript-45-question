ca437270e91f6fd0ebfde43eec1384a12733d1e2
Maidah208 <161070899+Maidah208@users.noreply.github.com>
2024-05-25 17:36:58 +0500

    Add files via upload

---
+++ Day 13/question37.ts
@@ -0,0 +1,6 @@
+function makeShirt(size:string = 'large' ,message:string = 'I Love Typescript'){
+    console.log(`The size of the shirt is '${size}' and the message on the shirt is '${message}'.`);
+}
+makeShirt('medium','hello world');
+makeShirt();
+makeShirt('small');
---
+++ Day 13/question38.ts
@@ -0,0 +1,6 @@
+function describe_city(city:string,country:string = 'Pakistan'){
+    console.log(`${city} is in ${country}`);
+}
+describe_city('Karachi');
+describe_city('Paris','France');
+describe_city('New York','USA');
