2ebaa3601a25d1b6e8f096a769c690de30dd19a1
Maidah208 <161070899+Maidah208@users.noreply.github.com>
2024-05-25 17:45:40 +0500

    Delete 36-37.ts

--- 36-37.ts
+++
@@ -1,7 +0,0 @@
-function make_shirt(size:string = 'large' ,message:string = 'I Love Typescript'){
-    console.log('The size of the shirt is \'' + size + '\' and the message on the shirt is \'' + message + '\'');
-}
-
-make_shirt('medium','hello world');
-make_shirt();
-make_shirt('small');
