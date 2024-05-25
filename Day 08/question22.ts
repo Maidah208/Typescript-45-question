d2587983eabde99ef4994100a07b8801a9dbeab1
Maidah208 <161070899+Maidah208@users.noreply.github.com>
2024-05-25 17:34:46 +0500

    Add files via upload

---
+++ Day 08/question22.ts
@@ -0,0 +1,6 @@
+let friends = ['Maidah','khudaja','Mahnoor'];
+// Trying to access an index that doesn't exist (index 3)
+console.log(friends[3]);
+//correcting the error
+friends.push('Mirha');
+console.log(friends[3]);
