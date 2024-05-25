7f2b3c1508ce5ce493952d7af4a0ed20f8fc6a7c
Maidah208 <161070899+Maidah208@users.noreply.github.com>
2024-05-25 17:42:48 +0500

    Delete 42.ts

--- 42.ts
+++
@@ -1,7 +0,0 @@
-let Magicians = ['Harry','David','Teller','Criss'];
-function make_great(magician: string[]){
-    for (let magician of Magicians){
-        console.log( 'Hello ' + magician);
-    }
-}
-make_great(Magicians);
