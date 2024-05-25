e8af70114332c4c0d4b799fc63af49f236741759
Maidah208 <161070899+Maidah208@users.noreply.github.com>
2024-05-25 17:37:19 +0500

    Add files via upload

---
+++ Day 14/question41.ts
@@ -0,0 +1,7 @@
+let magicians = ['Harry','David','Teller','Criss'];
+function show_magicians(magician: string[]){
+    for (let magician of magicians){
+        console.log(magician);
+    }
+}
+show_magicians(magicians);
---
+++ Day 14/question42.ts
@@ -0,0 +1,6 @@
+function make_great(magician: string[]){
+    for (let magician of magicians){
+        console.log( 'Hello ' + magician);
+    }
+}
+make_great(magicians);
