13f54526f996450459ec5b8ca80ded3682c24c13
Maidah208 <161070899+Maidah208@users.noreply.github.com>
2024-05-25 17:35:18 +0500

    Add files via upload

---
+++ Day 09/question25.ts
@@ -0,0 +1,8 @@
+let alien_color = 'green';
+if (alien_color=='green'){
+    console.log('Congratulations! You get 5 points');
+}
+alien_color = 'red'
+if (alien_color=='green'){
+    // no Output because the condition is false.
+}
---
+++ Day 09/question26.ts
@@ -0,0 +1,15 @@
+let alienColor = 'green';
+if (alienColor == 'green'){
+    console.log('Congratulations! You just earned 5 points for shooting the  green alien.');
+}
+else {
+    console.log('Congratulations! You just earned 10 points for shooting other than the  green alien.')
+}
+//when alien is other than green color
+alienColor = 'red';
+if (alienColor == 'green'){
+    console.log('Congratulations! You just earned 5 points for shooting the  green alien.');
+}
+else {
+    console.log('Congratulations! You just earned 10 points for shooting other than the  green alien.')
+}
