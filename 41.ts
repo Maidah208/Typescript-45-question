561c4c236ef6078261f8f727de2769d2c51f3455
Maidah208 <161070899+Maidah208@users.noreply.github.com>
2024-05-25 17:43:04 +0500

    Delete 41.ts

--- 41.ts
+++
@@ -1,9 +0,0 @@
-let magicians = ['Harry','David','Teller','Criss'];
-function show_magicians(magician: string[]){
-    for (let magician of magicians){
-        console.log(magician);
-    }
-}
-
-show_magicians(magicians);
-
