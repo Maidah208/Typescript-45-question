73592fa36a98a75865d0c5f62ecacd4be60d3c03
Maidah208 <161070899+Maidah208@users.noreply.github.com>
2024-05-25 17:43:52 +0500

    Delete 38.ts

--- 38.ts
+++
@@ -1,7 +0,0 @@
-function describe_city(city:string,country:string = 'Pakistan'){
-    console.log(city + ' is in ' + country);
-}
-
-describe_city('Karachi');
-describe_city('Paris','France');
-describe_city('New York','USA');
