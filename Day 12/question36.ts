4fbf7914cc56e93ca9ce37631b0fa0051c1d1db5
Maidah208 <161070899+Maidah208@users.noreply.github.com>
2024-05-25 17:36:37 +0500

    Add files via upload

---
+++ Day 12/question36.ts
@@ -0,0 +1,4 @@
+function make_shirt(size:string, message:string){
+    console.log(`The size of the shirt is ${size} and the message on the shirt is ${message}.`);
+}
+make_shirt('medium','hello world');
